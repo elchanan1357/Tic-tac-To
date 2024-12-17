2d98e9e8eb581f41896b8f9193e570c7ac30328c
unknown <alhanan055224@gmail.com>
2024-12-17 19:43:33 +0200

    build a borad game with all function

--- app/src/main/java/com/example/tictacto/Activity2.kt
+++ app/src/main/java/com/example/tictacto/Activity2.kt
@@ -1,45 +1,84 @@
 package com.example.tictacto
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class Activity2 : AppCompatActivity() {
-    var boardGame:ArrayList<ImageButton> = ArrayList();
+    private var boardGame: ArrayList<ImageButton> = ArrayList(9)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
-        getBoard();
+        setTitle("                       Tic Tac To")
 
+        val board: LinearLayout = findViewById(R.id.linearLayout);
+        for (i in 0 until 9)
+            this.boardGame.add(board.findViewWithTag(i.toString()))
 
+        this.boardGame.forEachIndexed { index, imageButton ->
+            imageButton.setOnClickListener { onClick(imageButton) }
+        }
+
+        findViewById<Button?>(R.id.Activity2_reset).setOnClickListener { finish() }
     }
 
-    private fun getBoard(){
-        //לשנות את זה שזה יעבוד לפי tag ולא לפי id
-        for (i in 0 until 9) {
-            var imageButtonID: Int =
-                resources.getIdentifier("Activity2_box$i", "id", packageName);//find id dynamic
-            if (imageButtonID != 0) {
-                boardGame.add(findViewById(imageButtonID));
-            }
-        }
+    private var turn: Boolean = true
 
-        boardGame.forEachIndexed{ index,imageButton->
-            imageButton.setOnClickListener{
-                click(index,imageButton)
-        }
-        }
-    }
+    @SuppressLint("SetTextI18n")
+    private fun onClick(btn: ImageButton) {
+        if (btn.drawable != null) return
 
-    //צריך לטפל בלחיצה כפולה שמשנה את הרקע
-    var first:Boolean = true
-    private fun click(i:Int ,imageButton: ImageButton){
-        if (first)
-             imageButton.setImageResource(R.drawable.x);
+        if (this.turn)
+            btn.setImageResource(R.drawable.x)
         else
-             imageButton.setImageResource(R.drawable.circle);
+            btn.setImageResource(R.drawable.circle)
 
-        first = !first
+        this.turn = !this.turn
+
+
+        if (win()) endOfGame(!turn)
+    }
+
+    private fun win(): Boolean {
+        val rowsWin = listOf(
+            listOf(0, 1, 2),
+            listOf(3, 4, 5),
+            listOf(6, 7, 8),
+            listOf(0, 3, 6),
+            listOf(1, 4, 7),
+            listOf(2, 5, 8),
+            listOf(0, 4, 8),
+            listOf(2, 4, 6)
+        )
+
+        for (row in rowsWin) {
+            val state1 = this.boardGame[row[0]].drawable?.constantState
+            val state2 = this.boardGame[row[1]].drawable?.constantState
+            val state3 = this.boardGame[row[2]].drawable?.constantState
+            if (state1 != null && state1 == state2 && state2 == state3)
+                return true;
+        }
+
+        return false;
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun endOfGame(win: Boolean) {
+        val name: String? = intent.getStringExtra("Name${if (win) 1 else 2}")
+
+        val winner: TextView = findViewById(R.id.Activity2_TextWin)
+        winner.setBackgroundColor(Color.parseColor("#E91E63"))
+        winner.setTextColor(Color.parseColor("#BCF481"))
+        winner.text = "The $name is a Winner"
+
+        this.boardGame.forEachIndexed { index, imageButton ->
+            imageButton.isClickable = false
+        }
     }
 }
--- app/src/main/java/com/example/tictacto/MainActivity.kt
+++ app/src/main/java/com/example/tictacto/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.tictacto
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -13,22 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        setTitle("Welcome To The Tic Tac To")
-//        supportActionBar?.setDisplayOptions(ActionBar.DISPLAY_SHOW_TITLE, ActionBar.DISPLAY_SHOW_TITLE);
-//        supportActionBar?.setTitle("new title")
-//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
+        setTitle("          Welcome To The Tic Tac To")
 
         val startGame: Button = findViewById(R.id.MainActivity_StartBtn)
         startGame.setOnClickListener(::login)
     }
 
-   private fun login(view: View){
-        val player1:String = findViewById<EditText?>(R.id.Main_player1).text.toString()
-        val player2:String = findViewById<EditText?>(R.id.Main_player2).text.toString()
+    private fun login(view: View) {
+        val player1: String = findViewById<EditText?>(R.id.Main_player1).text.toString()
+        val player2: String = findViewById<EditText?>(R.id.Main_player2).text.toString()
 
-        if(player1.isNotEmpty() && player2.isNotEmpty()){
-            val intent =  Intent(this,Activity2::class.java)
+        if (player1.isNotEmpty() && player2.isNotEmpty()) {
+            val intent = Intent(this, Activity2::class.java)
+            intent.putExtra("Name1", player1)
+            intent.putExtra("Name2", player2)
             startActivity(intent)
         }
 
