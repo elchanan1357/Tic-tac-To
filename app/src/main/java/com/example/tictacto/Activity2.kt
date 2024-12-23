e4522637be8a69415d069f4f8f18db7e9ad8a827
unknown <alhanan055224@gmail.com>
2024-12-23 20:19:56 +0200

    display user message

--- app/src/main/java/com/example/tictacto/Activity2.kt
+++ app/src/main/java/com/example/tictacto/Activity2.kt
@@ -3,6 +3,7 @@ package com.example.tictacto
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
@@ -11,11 +12,19 @@ import androidx.appcompat.app.AppCompatActivity
 
 class Activity2 : AppCompatActivity() {
     private var boardGame: ArrayList<ImageButton> = ArrayList(9)
+    private var name1: String? = null
+    private var name2: String? = null
+    private var textGame: TextView? = null
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
-        setTitle("                       Tic Tac To")
+        textGame = findViewById(R.id.Activity2_textOfFlow)
+        name1 = intent.getStringExtra("Name1")
+        name2 = intent.getStringExtra("Name2")
+        textGame?.text = "$name1 play"
 
         val board: LinearLayout = findViewById(R.id.linearLayout);
         for (i in 0 until 9)
@@ -29,9 +38,12 @@ class Activity2 : AppCompatActivity() {
     }
 
     private var turn: Boolean = true
+    private var counter = 0
 
     @SuppressLint("SetTextI18n")
     private fun onClick(btn: ImageButton) {
+        var name: String? = if (!turn) name1 else name2
+
         if (btn.drawable != null) return
 
         if (this.turn)
@@ -40,9 +52,12 @@ class Activity2 : AppCompatActivity() {
             btn.setImageResource(R.drawable.circle)
 
         this.turn = !this.turn
+        this.counter++
+        textGame?.text = "$name play"
 
-
-        if (win()) endOfGame(!turn)
+        name = if (!turn) name1 else name2
+        if (win()) endOfGame(name)
+        if (counter == 9) endOfGame()
     }
 
     private fun win(): Boolean {
@@ -69,16 +84,23 @@ class Activity2 : AppCompatActivity() {
     }
 
     @SuppressLint("SetTextI18n")
-    private fun endOfGame(win: Boolean) {
-        val name: String? = intent.getStringExtra("Name${if (win) 1 else 2}")
+    private fun endOfGame(win: String? = null) {
+        textGame?.setBackgroundColor(Color.parseColor("#E91E63"))
+        textGame?.setTextColor(Color.parseColor("#BCF481"))
 
-        val winner: TextView = findViewById(R.id.Activity2_TextWin)
-        winner.setBackgroundColor(Color.parseColor("#E91E63"))
-        winner.setTextColor(Color.parseColor("#BCF481"))
-        winner.text = "The $name is a Winner"
+        if (win == null) {
+            textGame?.text = "$name1 and $name2 is the draw"
+            return
+        }
+        if (win == name1)
+            textGame?.text = "$name1 is the winner"
+        else
+            textGame?.text = "$name2 is the winner"
 
         this.boardGame.forEachIndexed { index, imageButton ->
             imageButton.isClickable = false
         }
+
+        findViewById<Button>(R.id.Activity2_reset).visibility = View.VISIBLE
     }
 }
