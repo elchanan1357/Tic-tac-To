5768fc908561f46a744fae5e45d26c0f61bbe2a1
unknown <alhanan055224@gmail.com>
2024-12-24 21:19:48 +0200

    add in login page message of required

--- app/src/main/java/com/example/tictacto/MainActivity.kt
+++ app/src/main/java/com/example/tictacto/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.tictacto
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -27,6 +30,13 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("Name1", player1)
             intent.putExtra("Name2", player2)
             startActivity(intent)
+        }else{
+            val err:TextView = findViewById(R.id.MainActivity_textError)
+            err.visibility = View.VISIBLE
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                err.visibility = View.GONE
+            },2000)
         }
 
     }
